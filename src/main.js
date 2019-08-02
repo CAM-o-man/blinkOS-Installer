05abc3bc3b9e3b4644e0862f92618e054ff2d3f2
Connor <cam-o-man@live.com>
2019-08-02 10:31:22 -0400

    Changes main.js to run the install script on window load

--- src/main.js
+++ src/main.js
@@ -1,29 +1,32 @@
 const shell = require('@cam-o-man/simpleshell').execute;
 
 async function runScript() {
-    const htmlout = document.getElementById("updates");
-    const htmlerr = document.getElementById("errors");
-    const htmlpath = document.getElementById("path");
+    //const htmlout = document.getElementById("updates");
+    //const htmlerr = document.getElementById("errors");
+    //const htmlpath = document.getElementById("path");
     let child = shell(`pwd`, []);
     for await (let data of child.stdout) {
         console.log(`Present Working Directory: ${data}`);
-        htmlpath.innerText = `Present Working Directory: ${data}`
+        //htmlpath.innerText = `Present Working Directory: ${data}`
     }
     child = shell('./adb devices', []);
     for await (let data of child.stdout) {
         console.log(`adb devices: ${data}`);
-        htmlout.innerText = `adb devices: ${data}`;
+        //htmlout.innerText = `adb devices: ${data}`;
     }
     child = shell('./adb reboot bootloader', []);
     for await (let data of child.stdout) {
         console.log(`Entering bootloader: ${data}`);
-        htmlout.innerText = `Entering bootloader: ${data}`;
+        //htmlout.innerText = `Entering bootloader: ${data}`;
     }
     child = shell('./fastboot', ['-w update', 'blinkOS.zip']);
     console.log("Sideloading and flashing blinkOS, please be patient.");
-    htmlout.innerText = "Sideloading and flashing blinkOS, please be patient.";
+    //htmlout.innerText = "Sideloading and flashing blinkOS, please be patient.";
     for await(let data of child.stdout) {
         console.log(`Flashing complete: ${data}.`);
-        htmlout.innerText = `Flashing complete: ${data}.`;
+        //htmlout.innerText = `Flashing complete: ${data}.`;
     }
 }
+
+// noinspection JSCheckFunctionSignatures
+window.onload(runScript());
